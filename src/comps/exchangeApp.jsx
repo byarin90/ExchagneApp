03db5f9552513d88617dafcd21fbe2c8136a606f
Yarin Bukai <72891249+byarin90@users.noreply.github.com>
2022-04-04 11:29:39 +0300

    ok

--- src/comps/exchangeApp.jsx
+++ src/comps/exchangeApp.jsx
@@ -4,11 +4,13 @@ import ScoreEX from './scoreEX'
 import './exchangeApp.css'
 export default function ExchangeApp(props) {
   return (
-    <div style={{padding:'0',margin:'auto'}}>
-      <div className="strip">
+    <div className='AppEX'  style={{padding:'0',margin:'auto'
+    }}>
+           <div className="strip">
         <h1 className='display-1 text-white text-center text-shadow' style={{fontWeight:'bold'}}>Exchange Currency App</h1>
       </div>
-      <main className='row'>
+
+      <main className='row mx-auto'>
       <InputEX/>
       <ScoreEX/>
       </main>
--- src/comps/scoreEX.jsx
+++ src/comps/scoreEX.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function ScoreEX() {
   return (
-    <div className='col-md-7 scoreEX'>ScoreEX</div>
+    <div style={{}} className='col-md-7 scoreEX'>ScoreEX</div>
   )
 }
